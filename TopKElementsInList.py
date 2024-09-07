61ecbf6005e4dd4aed2253b134ff7001d4d5fd9c
Masuo-4 <masurotohi@gmail.com>
2024-09-07 13:03:55 +0000

    TopKElements1周目解き直し

--- TopKElementsInList.py
+++ TopKElementsInList.py
@@ -1,8 +1,17 @@
 from collections import defaultdict
+# バケットソートを使う
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = defaultdict(int)
+        freq = [[] for _ in range(len(nums) + 1)]      
         for num in nums:
             count[num] += 1
-        sorted_list =sorted(count.items(), key=lambda item: item[1], reverse=True)
-        return [key for key, value in sorted_list[:k]]
+        for num, count in count.items():
+            freq[count].append(num)
+        res = []
+        for num in reversed(freq):
+            # リストの連結
+            res.extend(num)
+            if len(res) == k:
+                return res
+            
