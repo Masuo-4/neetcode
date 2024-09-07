cd4f46cdd37466c9a47588b92a6bab0052d5de3f
Masuo-4 <masurotohi@gmail.com>
2024-09-07 15:59:38 +0000

    product一周目解き直し

--- Arrays&Hashing/ProductsofArrayDiscludingSelf.py
+++ Arrays&Hashing/ProductsofArrayDiscludingSelf.py
@@ -1,17 +1,15 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         res = [1] * len(nums)
-
-        for i, num in enumerate(nums):
-            if i == 0: continue
-            res[i] = res[i - 1] * nums[i - 1]
+        
+        prefix = 1
+        for i in range(num):
+            res[i] = prefix
+            prefix *= nums[i]
         
         postfix = 1
-        for i, num in reversed(list(enumerate(nums))):
-            if i == len(nums) - 1: continue
-            res[i + 1] *= postfix
-            print(res)
-            postfix *= nums[i + 1]
-        res[0] *= postfix
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
         
         return res
