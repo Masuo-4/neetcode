674bb0d0461b7d809cada18ff3e402495594b9d4
Masuo-4 <masurotohi@gmail.com>
2024-09-07 08:34:12 +0000

    TwoIntegerSum1周目解き直し

--- Arrays&Hashing/TwoIntegerSum.py
+++ Arrays&Hashing/TwoIntegerSum.py
@@ -2,9 +2,11 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         num_dict = dict()
         for i, num in enumerate(nums):  
-            num_dict[target - num] = i
-        for i, num in enumerate(nums):
+            diff = target - num
             if num in num_dict:
-                if i != num_dict[num]:
-                    return [i, num_dict[num]]
-        return []
+                return [num_dict[num], i]
+            else: num_dict[diff] = i
+
+# 自分のことを必要としている数があるかをnum_dictを見て探す。
+# あるならnum_dictのkeyがその数がある場所なので、return [相手の場所(num_dict[num]), 自分の場所(i)]
+# ないなら自分が求めてる数(target - num)をkeyに、自分の場所(i)をvalueにしてnum_dictに追加する。
