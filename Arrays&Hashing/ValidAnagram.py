a996d290915b2f238641049954745297415ca4a3
Masuo-4 <masurotohi@gmail.com>
2024-09-07 07:27:17 +0000

    validanagram1周目解き直し

--- Arrays&Hashing/ValidAnagram.py
+++ Arrays&Hashing/ValidAnagram.py
@@ -1,19 +1,17 @@
+from collections import defaultdict
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        spell_dict_s = dict()
+        s_count = defaultdict(int)
+        t_count = defaultdict(int)
+        if len(s) != len(t):
+            return False
+        
         for spell in s:
-            if spell in spell_dict_s:
-                spell_dict_s[spell] += 1
-            else:
-                spell_dict_s[spell] = 1
-                
-        spell_dict_t = dict()
+            s_count[spell] += 1
+            
         for spell in t:
-            if spell in spell_dict_t:
-                spell_dict_t[spell] += 1
-            else:
-                spell_dict_t[spell] = 1
-        
-        return spell_dict_t == spell_dict_s
-        
+            t_count[spell] += 1
+
+        return t_count == s_count
         
