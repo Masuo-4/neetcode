2fddfe869ed7668170da572e49db512ecca9c1cc
Masuo-4 <masurotohi@gmail.com>
2024-09-07 11:57:28 +0000

    topKElements初見解答

--- AnagramGroups.py
+++ AnagramGroups.py
@@ -1,17 +1,8 @@
 from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        group_dict = dict()
+        group_dict = defaultdict(list)
         for word in strs:
-            count_dict = defaultdict(int)
-            for s in word:
-                count_dict[s] += 1
-            count_dict = sorted(count_dict.items())
-            # listやdictは辞書のキーには使えない
-            count_dict_tuple = tuple(count_dict)
-            if count_dict_tuple in group_dict:
-                group_dict[count_dict_tuple].append(word)
-            else:
-                group_dict[count_dict_tuple] = [word]
-        
+            sorted_word = ''.join(sorted(word)) # str(sorted(word))は遅い
+            group_dict[sorted_word].append(word)
         return list(group_dict.values())
