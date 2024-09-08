148e5f51b0060a08209a6972fe69f811ef04ea13
Masuo-4 <masurotohi@gmail.com>
2024-09-08 09:44:16 +0000

    LongestConsecutive1周目解き直し

--- Arrays&Hashing/LongestConsecutiveSequence.py
+++ Arrays&Hashing/LongestConsecutiveSequence.py
@@ -4,17 +4,10 @@ class Solution:
         longest = 0
         for num in nums:
             # スタート位置ならば、連続する列の長さを求める。
-            if num - 1 not in nums_set:
-                search_num = num + 1
-                length = 1
-                while(search_num in nums_set):
-                    if num + 1 in nums_set:
+            if (num - 1) not in nums_set:
+                length = 0
+                while (num + length) in nums_set:
                         length += 1
-                        search_num += 1
-                    else: break
-                # 長さの最大値更新
-                longest = max(longest, length)
-            # スタート位置じゃないなら無視                  
-            else: continue
+                longest = max(length, longest)
         return longest
     
