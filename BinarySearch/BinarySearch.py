3499270356a141e31a9f1adedd2cec2e10c4ad68
🌵 <67841820+rotohi@users.noreply.github.com>
2024-09-15 10:08:05 +0000

    二周目

--- BinarySearch/BinarySearch.py
+++ BinarySearch/BinarySearch.py
@@ -1,14 +1,12 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r= 0, len(nums) - 1
-         
+        l, r = 0, len(nums) - 1
         while l <= r:
-            # overflowを防ぐ
             m = l + (r - l) // 2
             if nums[m] == target:
                 return m
-            elif nums[m] < target:
-                l = m + 1
-            elif target < nums[m]:
+            elif nums[m] > target:
                 r = m - 1
+            else:
+                l = m + 1
         return -1
--- BinarySearch/KokoEatingBananas.py
+++ BinarySearch/KokoEatingBananas.py
@@ -1,15 +1,16 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l, r = 1, max(piles)
-        res = r
+        max_pile = max(piles)
+        l, r = 1, max_pile
+        res = max_pile
         while l <= r:
             time = 0
             m = l + (r - l) // 2
-            for num in piles:
-                time += math.ceil(float(num) / m)
-            if time <= h:
+            for pile in piles:
+                time += math.ceil(float(pile)/m)
+            if time > h:
+                l = m + 1
+            else:
                 res = m
                 r = m - 1
-            elif time > h:
-                l = m + 1 
         return res
