aa51873fcd00645512bd2339f73880d6b91ea5d2
🌵 <67841820+rotohi@users.noreply.github.com>
2024-09-22 08:35:11 +0000

    二周目

--- Stack/LargestRectangleInHistogram.py
+++ Stack/LargestRectangleInHistogram.py
@@ -6,12 +6,10 @@ class Solution:
             start = i
             while stack and stack[-1][1] > h:
                 index, height = stack.pop()
-                max_area = max(max_area, height * (i - index))
+                max_area = max(max_area, (i - index) * height)
                 start = index
             stack.append((start, h))
-
         
         for i, h in stack:
-            max_area = max(max_area, (len(heights) - i) * h)
-
+            max_area = max(max_area, h * (len(heights) - i))
         return max_area
