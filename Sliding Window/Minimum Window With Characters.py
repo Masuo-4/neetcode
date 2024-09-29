d506f18ff61b1fd4fdd4eac2bf9bbd989edfb120
Masuo-4 <masurotohi@gmail.com>
2024-09-29 09:59:22 +0000

    二周目

--- Sliding Window/Minimum Window With Characters.py
+++ Sliding Window/Minimum Window With Characters.py
@@ -1,26 +1,30 @@
-from collections import Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if t == "": return ""
-
-        have, need = 0, len(t)
-        count_t,  window_count = Counter(t), Counter()
-        res, resLen = [-1, -1], float("infinity")
+        t_count = collections.Counter(t)
+        s_count = Counter()
+        have, need = 0, len(t_count)
+        res, resLen = [-1, -1], float("inf") # res = [l, r]
         l = 0
+
         for r in range(len(s)):
-            window_count[s[r]] += 1
-            if s[r] in count_t and window_count[s[r]] == count_t[s[r]]:
+            c = s[r]
+            s_count[c] += 1
+
+            if c in t_count and s_count[c] == t_count[c]:
                 have += 1
             
             while have == need:
                 if r - l + 1 < resLen:
-                    res = [l, r]
                     resLen = r - l + 1
+                    res = [l, r]
+                
+                s_count[s[l]] -= 1
 
-                window_count[s[l]] -= 1 
-                if s[l] in count_t and window_count[s[l]] < count_t[s[l]]:
+                if s[l] in t_count and s_count[s[l]] < t_count[s[l]]:
                     have -= 1
                 l += 1
-        l, r = res
 
+        l, r = res
         return s[l: r + 1] if resLen != float("infinity") else ""
+        
